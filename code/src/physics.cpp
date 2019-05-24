104f823b028d16f4c696fb13ce4cbf35a5b0777a
Axel Castells Monllau <axelcastellsmonllau@gmail.com>
2019-05-24 12:48:43 +0200

    FUCKING WORKS

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -11,6 +11,8 @@
 #define DEG2RAD 0.0174532924
 #define RAD2DEG 57.29578
 
+#define SPHERE_DRAGC 0.47f
+
 #define SPHERE_IBODY_MATRIX(m, r) glm::mat3((m * (r*r)) * (2.f/5.f))
 
 #define SPHERES_COUNT 1
@@ -195,9 +197,19 @@ glm::vec3 computeBuoyancyForce(FluidSystem* FLSys, BuoyantSphere* BSph, float ac
 }
 
 glm::vec3 computeDragForce(FluidSystem* FLSys, BuoyantSphere* BSph, float accum_time) {
-	
+	FluidSystem& FS = *FLSys;
+	glm::vec3 drag(0);
+	for (int i = 0; i < FS.size(); i++) {
+		if (BSph->countPoints > 0) {
 
-	return glm::vec3(0);
+			glm::vec3 relativeVel = BSph->sphereVelocity - FS[i].kBlood;
+			glm::vec3 relativeVelMod = glm::vec3(relativeVel.x * glm::length(relativeVel), relativeVel.y * glm::length(relativeVel), relativeVel.z * glm::length(relativeVel));
+			
+			float values = -0.5*FS[i].density*SPHERE_DRAGC * glm::abs(BSph->yPoints / BSph->countPoints);
+			drag += values*relativeVelMod;
+		}
+	}
+	return drag;
 }
 
 void updateSphere(FluidSystem* FLSys, BuoyantSphere* BSph, float accum_time, float dt) {
@@ -205,7 +217,7 @@ void updateSphere(FluidSystem* FLSys, BuoyantSphere* BSph, float accum_time, flo
 	glm::vec3 force = computeBuoyancyForce(&flsys, &sph, accum_time);
 	glm::vec3 drag = computeDragForce(&flsys, &sph, accum_time);
 
-	BSph->sphereForce += force + GRAVITY_FORCE * GRAVITY_VECTOR*SPHERE_MASS;
+	BSph->sphereForce += force + drag + GRAVITY_FORCE * GRAVITY_VECTOR*SPHERE_MASS;
 	BSph->sphereVelocity = dt * BSph->sphereForce / SPHERE_MASS;
 	BSph->centerSphere = BSph->centerSphere + dt * BSph->sphereVelocity;
 
@@ -248,138 +260,6 @@ namespace System {
 		updateSphere(&flsys, &sph, CURRENT_TIME, dt);
 	}
 }
-
-//namespace Old
-//{
-//	//Mesh
-//	std::vector<glm::vec3> list;
-//	std::vector<glm::vec3> initPos;
-//	glm::vec3 kBlood = glm::vec3(0.0f, 0.0f, 0.5f);
-//	glm::vec3 y = glm::vec3(0.0f, 1.0f, 0.0f);
-//
-//	float time, t, w, A, kItalic, density, vSub;
-//
-//
-//	//Sphere
-//	
-//
-//	float* ParticlesToFloatPointer()
-//	{
-//		float* v = new float[list.size() * 3];
-//		for (int i = 0; i < list.size(); i++)
-//		{
-//			v[i * 3 + 0] = list[i].x;
-//			v[i * 3 + 1] = list[i].y;
-//			v[i * 3 + 2] = list[i].z;
-//		}
-//		return v;
-//	}
-//	void Init()
-//	{
-//		//Sumatori del temps de simulacio
-//		t = 0.0f;
-//
-//		//Freqüencia. Mantenim els valors de la W
-//		if (w < 1.0f)w = 5.0f;
-//
-//		//Amplitud de la onada
-//		A = 0.5f;
-//
-//		//La longitud del vector
-//		kItalic = kBlood.length();
-//
-//		//DensitatFluit
-//		density = 15.0f;
-//
-//		//Volum Desplasat
-//		vSub = 0.0f;
-//
-//		//Mesh
-//		list.clear();
-//		initPos.clear();
-//		//float x;
-//		//float z = -5.5f;
-//		//float y = 4.0f;
-//
-//		//for (int i = 0; i < Mesh::numRows; i++)
-//		//{
-//		//	
-//		//	//z += initalDistancePoints;
-//		//	//x = -5.5f;
-//		//	for (int j = 0; j < Mesh::numCols; j++)
-//		//	{
-//		//		glm::vec3 pointPos = getInitPos(i, j);
-//		//		//x += initalDistancePoints;
-//		//		//glm::vec3 p(x, y, z);
-//		//		list.push_back(pointPos);
-//		//		initPos.push_back(pointPos);
-//		//	}
-//		//}
-//		Mesh::updateMesh(ParticlesToFloatPointer());
-//
-//		//Sphere
-//		SPHERE_RADIUS = (rand() % 1) + 1;
-//		centerSphere.x = (rand() % 2) - 1;
-//		centerSphere.y = (rand() % 3) + 2;
-//		centerSphere.z = (rand() % 2) - 1;
-//		Sphere::updateSphere(centerSphere, SPHERE_RADIUS);
-//		sphereForce.y = -9.8f;
-//
-//	}
-//
-//	//Extret de la practica de rebot de les particules
-//	bool DistancePointToPoint(glm::vec3 point)
-//	{
-//		float distanciaPla = (centerSphere.y - SPHERE_RADIUS) - point.y;
-//		return distanciaPla <= 0;
-//	}
-//	void Update(float dt)
-//	{
-//		time += dt;
-//		if (time >= 15)
-//		{
-//			Init();
-//			time = 0;
-//		}
-//		else
-//		{
-//			//Accumulation
-//			t += dt;
-//			float yPoints = 0.0f;
-//			int countPoints = 0;
-//			glm::vec3 bouyancy;
-//
-//			for (int i = 0; i < list.size(); i++)
-//			{
-//				glm::vec3 v = (kBlood / kItalic)*A*sin(dot(kBlood, initPos[i]) - w * t);
-//				list[i].x = initPos[i].x - v.x;
-//				list[i].z = initPos[i].z - v.z;
-//
-//				list[i].y = (glm::vec3(A*cos(dot(kBlood, initPos[i]) - w * t))).y;
-//				if (DistancePointToPoint(list[i]))
-//				{
-//					yPoints += list[i].y;
-//					countPoints++;
-//				}
-//			}
-//
-//			//SPHERE
-//			//Semi-Implicit Euler
-//			if (countPoints > 0)
-//			{
-//				float d = (yPoints / countPoints) - (centerSphere.y - SPHERE_RADIUS);
-//				vSub = d * SPHERE_RADIUS*SPHERE_RADIUS;
-//				bouyancy = density * GRAVITY_FORCE * vSub*y;
-//
-//			}
-//			sphereForce = bouyancy + GRAVITY_FORCE*GRAVITY_VECTOR*SPHERE_MASS;
-//			sphereVelocity += dt * sphereForce / SPHERE_MASS;
-//			centerSphere = centerSphere + dt * sphereVelocity;
-//
-//			Sphere::updateSphere(centerSphere, SPHERE_RADIUS);
-//		}
-//	}
-////};
 
 
 // Boolean variables allow to show/hide the primitives
